b89672d678d00367aa3d72dd89f4b07a5ae6388e
vicsnet <adesanmiadebisi@gmail.com>
2022-10-22 12:12:04 +0100

    npm run build done

--- src/pages/HomeCard.jsx
+++ src/pages/HomeCard.jsx
@@ -20,37 +20,39 @@ const HomeCard = () => {
   }, []);
 
   return (
+    <div className="w-[100%] mx-auto bg-[#f5f7fa]">
+      <div className="flex w-[94%] mx-auto md:flex-row flex-col justify-between items-center">
+        <h1 className="text-[grey] text-[40px] cursor-pointer">PostDoc</h1>
+        <form>
+          <input
+            type="text"
+            placeholder="search Post"
+            value={search}
+            onChange={(e) => setSearch(e.target.value)}
+            className=" align-middle w-[265px] h-[2.5rem] outline-none border-[2px] rounded-[4px] pl-[4px] mx-auto"
+          />
+        </form>
+      </div>
       <div className="w-[90%] mx-auto bg-[#f5f7fa]">
-          <div className="flex  md:flex-row flex-col justify-between items-center">
-              <h1 className="text-[grey] text-[40px] cursor-pointer">PostDoc</h1>
-      <form>
-        <input
-          type="text"
-          placeholder="search Post"
-          value={search}
-                  onChange={(e) => setSearch(e.target.value)}
-                  className=" align-middle w-[265px] h-[2.5rem] outline-none border-[2px] rounded-[4px] pl-[4px] mx-auto"
-        />
-      </form>
-          </div>
-      <div className="flex md:flex-row flex-wrap flex-col gap-8 mt-[20px]">
-        {users
-          .filter((user) => {
-            return search.toLowerCase() === ""
-              ? user
-              : user.title.toLowerCase().includes(search);
-          })
-          .map((user) => (
-            <div
-              className="mx-auto w-[80%] md:w-[40%] p-[8px] bg-[white] py-[30px] rounded-[10px] shadow-xl"
-              key={user.id}
-            >
-              <h1 className="text-[grey] text-[18px] text-center">
-                {user.title}
-              </h1>
-              <p className="text-[14px] text-center">{user.body}</p>
-            </div>
-          ))}
+        <div className="flex md:flex-row flex-wrap flex-col gap-8 mt-[20px]">
+          {users
+            .filter((user) => {
+              return search.toLowerCase() === ""
+                ? user
+                : user.title.toLowerCase().includes(search);
+            })
+            .map((user) => (
+              <div
+                className="mx-auto w-[80%] md:w-[40%] p-[8px] bg-[white] py-[30px] rounded-[10px] shadow-xl"
+                key={user.id}
+              >
+                <h1 className="text-[grey] text-[18px] text-center">
+                  {user.title}
+                </h1>
+                <p className="text-[14px] text-center">{user.body}</p>
+              </div>
+            ))}
+        </div>
       </div>
     </div>
   );
